931eda8eac74cfc5aece32ca4078dac2dc0ca6ad
Cole Campbell <cole.william.campbell@gmail.com>
2019-12-10 20:57:07 +0000

    Basic Routes
    Added basic routes. Calling to them from template using named routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate";
+import Event from "../views/Event";
+import EventList from "../views/EventList";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "eventList",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/create",
+    name: "eventCreate",
+    component: EventCreate
+  },
+  {
+    path: "/event",
+    name: "event",
+    component: Event
   }
 ];
 
